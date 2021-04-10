950991c64872d89be8c4861ccad1833d2d182d78
Hugo Haldi <hugo.haldi@etu.unige.ch>
2021-04-10 23:46:26 +0200

    Removed old files

--- LayerImageEntry.py
+++
@@ -1,8 +0,0 @@
-from ImageEntry import ImageEntry
-from PySide6.QtWidgets import QWidget
-
-
-class LayerImageEntry(ImageEntry):
-    def __init__(self, parent: QWidget, image, path, name):
-        super(LayerImageEntry, self).__init__(parent, image, path, name)
-
