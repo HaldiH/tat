c2ed99626cb3615ab8ff6222ff53a8d3145a557c
Hugo Haldi <hugo.haldi@etu.unige.ch>
2021-04-11 01:12:28 +0200

    Remove debug output

--- src/PreviewWindow.py
+++ src/PreviewWindow.py
@@ -50,7 +50,6 @@ class PreviewWindow(QMainWindow):
         self._selected_image_entry = None
 
     def image_entry_click_handler(self, sender: ImageEntry, event: QMouseEvent) -> None:
-        print(f"image entry selected: {sender.image_path}")
         self.set_preview_image(load_image(sender.image_path), sender)
 
     def resizeEvent(self, event: QResizeEvent) -> None:
