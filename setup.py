e9cea492f469508a23174dbae95b308284233269
Hugo Haldi <hugo.haldi@etu.unige.ch>
2021-05-13 14:34:41 +0200

    Add desktop file for *NIX platforms

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+import os
+from typing import List, Tuple
+
 import setuptools
+
+data_files: List[Tuple[str, List[str]]] = []
+
+if os.name == 'posix':
+    data_files.append(('share/applications', ['data/ch.unige.tat.desktop']))
 
 setuptools.setup(
     setup_requires=['pbr'],
-    pbr=True
+    pbr=True,
+    data_files=data_files
 )
