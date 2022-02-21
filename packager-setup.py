b75dd60214bc2c44f51c647d68f528e413c57a43
Hugo Haldi <20846785+ShinoYasx@users.noreply.github.com>
2022-02-21 19:53:01 +0100

    Trying to build only on Windows

--- packager-setup.py
+++ packager-setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 from pbr.packaging import get_version
 
-build_exe_options = {"packages": [], "excludes": []}
+build_exe_options = {"packages": ["PySide6", "cv2", "skimage"], "excludes": []}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
@@ -14,5 +14,5 @@ setup(
     version="{}.{}.{}".format(*get_version("tat").split(".")),
     description="Tomography Analysis Tool",
     options={"build_exe": build_exe_options},
-    executables=[Executable("__main__.py", base=base, target_name="tat")]
+    executables=[Executable("src/tat/__main__.py", base=base, target_name="tat")]
 )
