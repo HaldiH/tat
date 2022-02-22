db50c1d3a9259bda8804b34a7531108cfa4ba657
Hugo Haldi <20846785+ShinoYasx@users.noreply.github.com>
2022-02-22 16:57:55 +0100

    Test buildind Windows installer on pipeline

--- packager-setup.py
+++ packager-setup.py
@@ -2,17 +2,30 @@ import sys
 from cx_Freeze import setup, Executable
 from pbr.packaging import get_version
 
-build_exe_options = {"packages": ["PySide6", "cv2", "skimage"], "excludes": []}
+build_exe_options = {
+    "packages": [
+        "PySide6",
+        "cv2",
+        "skimage"
+    ],
+    "excludes": [
+        "tkinter"
+    ]}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+version = ""
+with open("version.txt") as f:
+    version = f.read().rstrip("\n")
+
 setup(
     name="tat",
-    version="{}.{}.{}".format(*get_version("tat").split(".")),
+    version=version,
     description="Tomography Analysis Tool",
     options={"build_exe": build_exe_options},
-    executables=[Executable("src/tat/__main__.py", base=base, target_name="tat")]
+    executables=[Executable("src/tat/__main__.py",
+                            base=base, target_name="tat")]
 )
